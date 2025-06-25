255d6153662dc8a8ed2273ae374c68ba611a80bd
romeu cajamba <romeucajamba@gmail.com>
2025-06-25 17:17:56 +0100

    refat: refatoração de icones

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,22 +1,11 @@
-
-import { Settings } from "lucide-react";
-import { HiAcademicCap } from "react-icons/hi2";
-import { HiChatBubbleOvalLeft } from "react-icons/hi2";
-import { LuCalendarRange } from "react-icons/lu";
-import { GrHomeOption } from "react-icons/gr";
-import { CiUser } from "react-icons/ci";
-import { FaLaptopCode } from "react-icons/fa";
-
-
-
-
+import { Settings, LuCalendarRange, GrHomeOption, CiUser, FaLaptopCode, BsChat, HiOutlineAcademicCap } from "@/lib/icons";
 
 export const routes = [
   { id: "home", icon: GrHomeOption, title: "Home", url:"/dashboard" },
   { id: "agenda", icon: LuCalendarRange, title: "Agenda", url:"/gender" },
   { id: "espaço de trabalho", icon: FaLaptopCode, title: "Meus espaço", url:"/workspace" },
-  { id: "conversas", icon: HiChatBubbleOvalLeft, title: "Conversas", hasNotification: true, url:"/chat" },
-  { id: "carreira", icon: HiAcademicCap, title: "Carreira", url:"/carrer" },
+  { id: "conversas", icon: BsChat, title: "Conversas", hasNotification: true, url:"/chat" },
+  { id: "carreira", icon: HiOutlineAcademicCap, title: "Carreira", url:"/carrer" },
   { id: "perfil", icon: CiUser, title: "Perfil", url:"/profile" },
   { id: "definicoes", icon: Settings, title: "Definições", url:"/settings" },
 ];
